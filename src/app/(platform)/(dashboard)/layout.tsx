0e2610c84f67f23848132a7a2809986177f13134
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-12-08 16:51:01 +0545

    feat: clerk organizations and middlewares

---
+++ src/app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/dashboard/navbar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
