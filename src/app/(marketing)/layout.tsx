af3fac0d08e1ee257c5852c3a6b2b0d63cb10069
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-12-08 15:51:50 +0545

    feat: navbar, footer, logo

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,10 +1,13 @@
+import { Footer } from "@/components/marketing/footer";
+import { Navbar } from "@/components/marketing/navbar";
+
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
-      {/* Navbar  */}
-      <main className="pt-40 pb-20 bg-slate-100">
+      <Navbar />
+      <main className="pt-20 pb-20 bg-slate-100">
         {children}
-        {/* Footer */}
+        <Footer />
       </main>
     </div>
   );
---
+++ src/components/logo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../../public/font/font.woff2",
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src="/logo.svg" alt="logo" width={30} height={30} />
+        <div className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}>Taskify</div>
+      </div>
+    </Link>
+  );
+};
