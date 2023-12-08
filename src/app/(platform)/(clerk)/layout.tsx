a28ec6294579a5b161da6bc62394f97ea2463c48
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-12-08 16:13:52 +0545

    feat: clerk authentication

---
+++ src/app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,5 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full flex justify-center items-center">{children}</div>;
+};
+
+export default ClerkLayout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { siteConfig } from "@/config/site";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { siteConfig } from "@/config/site";
 
 const inter = Inter({ subsets: ["latin"] });
 
