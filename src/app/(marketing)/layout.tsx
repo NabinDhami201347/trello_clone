c2ef589dee8fb0ded84bd44e32ddc95a7d940206
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-12-08 15:30:56 +0545

    feat: marketing page with custom font

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      {/* Navbar  */}
+      <main className="pt-40 pb-20 bg-slate-100">
+        {children}
+        {/* Footer */}
+      </main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
