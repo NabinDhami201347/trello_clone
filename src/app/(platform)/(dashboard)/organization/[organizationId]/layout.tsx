6f97e6455695c94664d5a18e28d20bf8ff544d57
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-12-09 12:04:07 +0545

    feat: updated title for organization

--- src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
+
 import { OrgControl } from "@/components/organization/org-control";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
