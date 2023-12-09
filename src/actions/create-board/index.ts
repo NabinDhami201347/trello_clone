9a182062d90d33ce0a12364b90c01af8d2a2afc1
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-12-09 10:20:43 +0545

    feat: useAction hook

---
+++ src/actions/create-board/index.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+
+import { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
+
+import { InputType, ReturnType } from "./types";
+import { CreateBoard } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { title, image } = data;
+
+  let board;
+
+  try {
+    board = await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to create.",
+    };
+  }
+
+  revalidatePath(`/board/${board.id}`);
+  return { data: board };
+};
+
+export const createBoard = createSafeAction(CreateBoard, handler);
